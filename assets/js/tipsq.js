ef675f12ea9916b4650b3ea91b05f1861c34b376
Miya <ciutirc@gmail.com>
2016-05-03 08:45:24 +0700

    Update tipsq.js

--- assets/js/tipsq.js
+++ assets/js/tipsq.js
@@ -9,6 +9,11 @@ QMiya[0] = "Jangan menertawakan impian orang lain <i>mimin</i>";
 QMiya[1] = "Ajarkan walau satu ayat";
 QMiya[2] = "Jangan merasa bangga dengan apa yang kamu bisa dan punyai, sesungguhnya di atas langit masih ada langit";
 QMiya[3] = "Perkara yang paling sedikit di dunia ini ialah mendengar dan memahami apa yang anda dengar";
+QMiya[4] = "Start with reading <i>Miya</i>";
+QMiya[5] = "Kuncinya adalah: Baca, Pahami, Ikuti <i>Miya</i>";
+QMiya[6] = "Orang yang tidak pernah membaca adalah orang yang tidak banyak tahu, orang yang tidak banyak tahu sangat dekat dengan kebodohan, dan kebodohan sangat dekat dengan kemiskinan <i>Tantowi Yahya</i>";
+
+
 
   var mf = QMiya.length;
     var whichQMiya=Math.round(Math.random()*(mf-1));
