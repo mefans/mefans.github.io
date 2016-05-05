7163f62d9a72d6d21c7e74ddacda0bf0db7b6194
Baguz Ach <eggoez@users.noreply.github.com>
2016-05-05 08:40:05 +0700

    menulis
    tambahan biar semangat nulis

--- assets/js/tipsq.js
+++ assets/js/tipsq.js
@@ -12,23 +12,32 @@ QMiya[3] = "Perkara yang paling sedikit di dunia ini ialah mendengar dan memaham
 QMiya[4] = "Start with reading <i>Miya</i>";
 QMiya[5] = "Kuncinya adalah: Baca, Pahami, Ikuti <i>Miya</i>";
 QMiya[6] = "Orang yang tidak pernah membaca adalah orang yang tidak banyak tahu, orang yang tidak banyak tahu sangat dekat dengan kebodohan, dan kebodohan sangat dekat dengan kemiskinan <i>Tantowi Yahya</i>";
-QMiya[] = " ";
-QMiya[] = " ";
-QMiya[] = " ";
-QMiya[] = " ";
-QMiya[] = " ";
-QMiya[] = " ";
-QMiya[] = " ";
-QMiya[] = " ";
-QMiya[] = " ";
-QMiya[] = " ";
-QMiya[] = " ";
-QMiya[] = " ";
-QMiya[] = " ";
-
-
-
-
+QMiya[7] = "Tips terbaik untuk menulis adalah dengan memulainya. Bismillah";
+QMiya[8] = "Kualitas seseorang bisa dilihat dari cara dia menulis dan mengeja";
+QMiya[9] = "Ikatlah ilmu dengan menulis. Karna ilmu yang ditulis akan tetap tersimpan diatas keterbatasan daya ingat manusia.";
+QMiya[10] = "Jangan menunggu terinspirasi baru menulis, tapi mulailah menulis sekarang, maka inspirasi akan datang menghampiri tulisanmu.";
+QMiya[11] = "Semua orang bisa menulis, tapi tidak semua orang bisa membuat tulisan.";
+QMiya[12] = "Menulis adalah bekerja untuk keabadian";
+QMiya[13] = "Membaca, menulis dan berbicara adalah ketiga hal yang saling terkait satu dengan lainnya. Persamaan ketiganya adalah ide.";
+QMiya[14] = "Orang boleh pandai setinggi langit, tapi selama ia tak menulis, ia akan hilang di dalam masyarakat & dari sejarah.<i> Pramoedya Ananta Toer</i>";
+QMiya[15] = "Menulis adalah suatu cara untuk bicara, suatu cara untuk menyapa, suatu cara untuk menyentuh seseorang yang lain entah di mana ia berada.";
+QMiya[16] = "Tulisan itu rekam jejak. Sekali dipublikasikan, tak kan bisa kau tarik. Menulis lah hal yang bermanfaat yang tak akan pernah kau sesali kemudian";
+QMiya[17] = "Menulis itu menenangkan pikiran dan nurani yang nyeri. <i>Helvy Tiana Rosa</i>";
+//QMiya[] = " ";
+//QMiya[] = " ";
+//QMiya[] = " ";
+//QMiya[] = " ";
+//QMiya[] = " ";
+//QMiya[] = " ";
+//QMiya[] = " ";
+//QMiya[] = " ";
+//QMiya[] = " ";
+//QMiya[] = " ";
+//QMiya[] = " ";
+//QMiya[] = " ";
+//QMiya[] = " ";
+//QMiya[] = " ";
+//QMiya[] = " ";
   var mf = QMiya.length;
     var whichQMiya=Math.round(Math.random()*(mf-1));
 function showQMiya(){document.write(QMiya[whichQMiya]);}
